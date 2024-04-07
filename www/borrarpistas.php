f9c463f3031b9c38e27f50c507a1b2c67086ffa0
Sabri Sanchez <sabri.sanchez.p@gmail.com>
2024-04-07 18:32:45 +0200

    Eliminación de paginas sin uso

--- www/borrarpistas.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once("database.php");
-$con = conectar();
-$codigos = $_POST['borrar'];
-borrar_pistas($con, $codigos);
-cerrar_conexion($con);
-header('Location: admin_page.php');
-?>
--- www/nuevapista.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once("database.php");
-$con = conectar();
-crear_pista($con, $_POST["nombre"]);
-cerrar_conexion($con);
-header("Location: admin_page.php");
-?>
