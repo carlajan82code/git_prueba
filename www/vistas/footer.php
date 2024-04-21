b4946b4e28409a33b710b1783546f9caf2ebe44a
Carla Janet Rojas <rojascarlajanet@gmail.com>
2024-04-21 18:57:28 +0200

    cambio icono de svg a png

--- www/vistas/footer.php
+++ www/vistas/footer.php
@@ -1,37 +1,36 @@
 <!DOCTYPE html>
 <footer class="footer">
 
-	<div class="footer_contenido">
-		<div class=footer_cotenido-logo>
+<div class="footer_contenido">
+			<div class=footer_cotenido-logo>
 
-			<img src="./img/seyfertDark.png" alt="logo Seyfert Footer" width="150px">
+				<img src="./img/seyfertDark.png" alt="logo Seyfert Footer" width="150px">
+			</div>
+			<div class="footer_contenido-contacto">
+				<h2>contacto</h2>
+				<p> Madrid, España</p>
+				<p> info@seyfert.com</p>
+				<p> +34 603565839</p>
+			</div>
+              <!--  iconos RRSS -->
+			  <div class="footer_redes">
+				<img src="./img/instagram.png" alt="red Instragram" href="">
+				<img src="./img/twiter.png" alt="red twiter" class="icono-twitter">
+				<img src="./img/linkedin.png" alt="red linkedin">
+				<img src="./img/whatsapp.png" alt="red whatsapp" class="icono-whatsapp">
+	
+			</div>
+
 		</div>
-		<div class="footer_contenido-contacto">
-			<h2>contacto</h2>
-			<p> Madrid, España</p>
-			<p> info@seyfert.com</p>
-			<p> +34 603565839</p>
+
+		<!-- Incluir una Linea -->
+		<span>
+
+		</span>
+
+		<div class="copi_footer">
+			<p>Copyright @<?php echo date("Y"); ?> All rights reserved by: SEYFERT</p>
 		</div>
-		<!--  iconos RRSS -->
-		<div class="footer_redes">
-			<img src="./img/instagram.svg" alt="red Instragram" href="">
-			<img src="./img/twiter.svg" alt="red twiter" class="icono-twitter">
-			<img src="./img/linkedin.svg" alt="red linkedin">
-			<img src="./img/whatsapp.svg" alt="red whatsapp" class="icono-whatsapp">
-
-		</div>
-
-	</div>
-
-	<!-- Incluir una Linea -->
-	<span>
-
-	</span>
-
-	<div class="copi_footer">
-		<p>Copyright @<?php echo date("Y"); ?> All rights reserved by: SEYFERT</p>
-	</div>
 </footer>
 </body>
-
 </html>
