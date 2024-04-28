0f8b274b79402c64c0ecfa83583f1facf7aac88a
Jorge_Sanchez <JorgeSR10@gmail.com>
2024-04-28 11:01:58 +0200

    cambios user_page

--- www/login.php
+++ www/login.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -8,7 +8,7 @@
     <title>Document</title>
 </head>
 
-<body>
+<body> -->
     <?php
     require_once("./vistas/header.php");
 
