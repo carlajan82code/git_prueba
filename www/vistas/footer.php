3f64d997aa2711f1b8d01532cbf73c2a80004841
Carla Janet Rojas <rojascarlajanet@gmail.com>
2024-04-28 19:27:20 +0200

    cambio error en footer

--- www/vistas/footer.php
+++ www/vistas/footer.php
@@ -1,36 +1,43 @@
 <!DOCTYPE html>
 <footer class="footer">
 
-<div class="footer_contenido">
-			<div class=footer_cotenido-logo>
+	<div class="footer_contenido">
+		<div class=footer_cotenido-logo>
 
-				<img src="./img/seyfertDark.png" alt="logo Seyfert Footer" width="150px">
-			</div>
-		
-		
-			<div class="footer_contenido-contacto">
-				<h2>contacto</h2>
-				<p> Madrid, España</p>
-				<p> info@seyfert.com</p>
-				<p> +34 603565839</p>
-			</div>
-              <!--  iconos RRSS -->
-			  <div class="footer_redes">
-				<img src="./img/instagram.png" alt="red Instragram" href="" width="50px">
-				<img src="./img/twiter.png" alt="red twiter"  width="50px">
-				<img src="./img/linkedin.png" alt="red linkedin" width="50px">
-				<img src="./img/whatsapp.png" alt="red whatsapp" class="icono-whatsapp" width="50px">
-	
-			</div>
-
-</div>
-
-		<!-- Incluir una Linea -->
-		
-
-		<div class="copi_footer">
-			<p>Copyright @<?php echo date("Y"); ?> All rights reserved by: SEYFERT</p>
+			<img src="./img/seyfertDark.png" alt="logo Seyfert Footer" width="150px">
 		</div>
+
+
+		<div class="footer_contenido-contacto">
+			<h2>contacto</h2>
+			<p> Madrid, España</p>
+			<p> info@seyfert.com</p>
+			<p> +34 603565839</p>
+		</div>
+		<!--  iconos RRSS -->
+		<div class="footer_redes">
+			<a href="https://www.instagram.com" target="_blank">
+				<img src="./img/instagram.png" alt="red Instragram" href="" width="50px"></a>
+
+				<a href="https://twitter.com" target="_blank">
+					<img src="./img/twiter.png" alt="red twiter" width="50px"></a>
+
+					<a href="https://linkedin.com" target="_blank">
+						<img src="./img/linkedin.png" alt="red linkedin" width="50px"></a>
+
+						<a href="https://whatsapp.com" target="_blank">
+							<img src="./img/whatsapp.png" alt="red whatsapp" class="icono-whatsapp" width="50px"></a>
+
+		</div>
+
+	</div>
+
+	<!-- Incluir una Linea -->
+
+	<div class="copi_footer">
+		<p>Copyright @<?php echo date("Y"); ?> All rights reserved by: SEYFERT</p>
+	</div>
 </footer>
 </body>
+
 </html>
