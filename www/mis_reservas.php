b718ba4cdcf00f1610f91c58e5068046046af912
Marco <marcomarquezb09@gmail.com>
2024-05-19 16:51:52 +0200

    Estilos en mis_reservas

--- www/mis_reservas.php
+++ www/mis_reservas.php
@@ -7,25 +7,37 @@ if (isset($_SESSION["id_usuario"])) {
 	die;
 }
 ?>
-<title>Mis Reservas</title>
-<h1>Mis Reservas</h1>
-<?php 
-require('database.php');
-$con = conectar();
-$resultado = obtener_mis_reservas($con, $_SESSION['id_usuario']);
-$num_filas = obtener_num_filas($resultado);
-if($num_filas == 0){
-    echo "No tienes reservas para hoy";
-}
 
-while($fila = obtener_resultados($resultado)){
-    extract($fila);
-    ?>
-    <div>
-        <label><?=$id_reserva?></label>
-        <label><?=$fecha?></label>
-        <label><?=$nombre?></label>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/styles/style.css">
+    <title>Mis reservas</title>
+</head>
+<body id="mis-reservas-body">
+    <title>Mis Reservas</title>
+    <h1>Mis Reservas</h1>
+    <div id="reservas-contenedor">
+        <div id="listado-reservas">
+            <?php 
+            require('database.php');
+            $con = conectar();
+            $resultado = obtener_mis_reservas($con, $_SESSION['id_usuario']);
+            $num_filas = obtener_num_filas($resultado);
+            if($num_filas == 0){
+                echo "No tienes reservas para hoy";
+            }
+    
+            while($fila = obtener_resultados($resultado)){
+                extract($fila);
+                ?>
+                <div>
+                    <label><strong><?=$fecha?></strong></label>
+                    <label><?=$nombre?></label>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
     </div>
-<?php
-}
-?>
+</body>
