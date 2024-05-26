aea0e42d70bcbecaadeb03ed647ec22ce93a96cd
Carla Janet Rojas <rojascarlajanet@gmail.com>
2024-05-26 16:41:57 +0200

    Etiquetas aria en headers

--- www/vistas/header.php
+++ www/vistas/header.php
@@ -15,15 +15,15 @@
 				<a href="./index.php"><img src="img/Seyfert.png" alt="logo Seyfert" width="150px"></a>
 
 				<div class="enlaces-nav">
-					<a class="navegacion_contenido--enlace" href="./registro.php">Registrarse</a>
-					<a class="navegacion_contenido--enlace" href="./login.php">Log-in</a>
-					<a class="navegacion_contenido--enlace" href="./galeria.php">Galeria</a>
+					<a class="navegacion_contenido--enlace" href="./registro.php" aria-label="Enlace a  Registro">Registrarse</a>
+					<a class="navegacion_contenido--enlace" href="./login.php" aria-label="Enlace a Login"v>Log-in</a>
+					<a class="navegacion_contenido--enlace" href="./galeria.php" aria-label="Enlace a Galeria">Galeria</a>
 
 				</div>
 				<!-- <a class="nav-link" href="../index.php?log-out">Log-out</a> -->
 
 				<div class="enlaces-nav__boton">
-					<a class="navegacion_contenido--enlace" href="contacto.php">Contacto</a>
+					<a class="navegacion_contenido--enlace" href="contacto.php" aria-label="Enlace a Contacto">Contacto</a>
 					<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ufo" width="42" height="42" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
 						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 						<path d="M16.95 9.01c3.02 .739 5.05 2.123 5.05 3.714c0 2.367 -4.48 4.276 -10 4.276s-10 -1.909 -10 -4.276c0 -1.59 2.04 -2.985 5.07 -3.724" />
@@ -35,7 +35,7 @@
 						<path d="M17 13h.01" />
 					</svg>
 
-					<a class="navegacion_contenido--boton" href="./blog.php">blog</a>
+					<a class="navegacion_contenido--boton" href="./blog.php" aria-label="Enlace a Blog">blog</a>
 
 				</div>
 
